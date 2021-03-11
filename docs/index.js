1d3be1028654fe2591bb78d8d8917104d6598e38
schwadan001 <schwadan001@gmail.com>
2021-03-11 13:18:38 -0600

    make packs collapsible

--- docs/index.js
+++ docs/index.js
@@ -1,11 +1,15 @@
+// Remove leading tms path for local testing. This is needed for GitHub Pages
 $.getJSON("/tms/tms.json", function (tms) {
+    // populate page with TMS verses
     var tms_id = document.getElementById("tms");
     var tms_text = "";
     var category_counter = 0;
 
     tms.categories.forEach(c => {
         category_counter++;
-        tms_text += "<br/><h2>" + c.name + "</h2>";
+        tms_text += '<button type="button" class="collapsible">' +
+            '<h2><span class="collapse-char">+</span> ' + c.name +
+            '</h2></button><div class="content">';
         var topic_counter = 0;
         c.topics.forEach(t => {
             tms_text += "<br/><h3>" + category_counter + "." +
@@ -14,7 +18,24 @@ $.getJSON("/tms/tms.json", function (tms) {
                 tms_text += v.text;
             });
         });
-        tms_text += "<br/>";
+        tms_text += "</div>";
     });
     tms_id.innerHTML = tms_text;
+
+    // add event listeners for expanding and collapsing topics
+    var e = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < e.length; i++) {
+        e[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+                this.getElementsByClassName("collapse-char")[0].innerHTML = "+"
+            } else {
+                content.style.display = "block";
+                this.getElementsByClassName("collapse-char")[0].innerHTML = "-"
+            }
+        });
+    }
 });
